326e9f365a9bc1b86919c65fe423f693af611a11
Janxyz11 <john.collado094@gmail.com>
2026-04-13 22:04:13 +0800

    Implemented add to cart functionality with quantity increment

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const cartItems = document.getElementById("cartItems");
 const cartPanel = document.getElementById("cartPanel");
 
 let cart = {};
+
+function addToCart(item) {
+    if (cart[item]) {
+        cart[item]++;
+    } else {
+        cart[item] = 1;
+    }
+
+    renderCart();
+
+    cartPanel.classList.add("show");
+}
